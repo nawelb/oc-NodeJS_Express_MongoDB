68f41675423747578e4eb030df61d14d17be5f18
BERQOUCH <5107270-NawelB@users.noreply.gitlab.com>
2020-12-13 19:59:31 +0100

    CRUD implementation

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,8 @@ const app= express();
 
 const connexionDb=process.env.DB_URL || DB_URL;
 
+const Thing = require('./models/Things');
+
  mongoose.connect(connexionDb,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
@@ -23,39 +25,51 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next(); 
-  });
+});
 
-  app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-      message: 'Objet créé !'
+app.post('/api/stuff', (req, res, next) => {
+// Delete _id from frontend
+    delete req.body._id;  
+    const thing = new Thing({
+       // L'opérateur spread ... est utilisé pour faire une copie de tous les éléments de req.body
+        ...req.body
     });
+    // save enregistre en db
+    thing.save()
+        .then(()=> res.status(200).json({message: 'Objet créé !'}))
+        .catch(error => res.status(400).json({ error }));  
+});
+
+app.get('/api/stuff/:_id', (req, res, next) => {
+    Thing.findOne({ _id: req.params._id })
+      .then(thing => res.status(200).json(thing))
+      .catch(error => res.status(404).json({ error }));
+});
+
+app.put('/api/stuff/:_id', (req, res, next) => {
+    Thing.updateOne({_id : req.params._id}, { ...req.body, _id:req.params._id})
+      .then(() => res.status(200).json({message : 'Objet modifié !'}))
+      .catch(error => res.status(400).json({error}));
   });
 
-  app.use('/api/stuff', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
+app.get('/api/stuff', (req, res, next) => {
+    Thing.find()
+        .then(things=>res.status(200).json(things))
+        .catch(error => res.status(400).json({ error }));
+});
+
+  app.get('/api/stuff/:title', (req, res, next) => {
+    Thing.findOne({ title: req.params.title })
+      .then(thing => res.status(200).json(thing))
+      .catch(error => res.status(404).json({ error }));
   });
 
-app.use((req, res) => {
-    res.json({ message : "Bien reçue ! "});
-})
+ 
 
-module.exports=app;
+  app.delete('/api/stuff/:id'), (req, res, next)=>{
+    Thing.deleteOne({_id:req.params.id})
+    .then(()=> res.status(200).json({message : 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({error}))
+  }
+
+  module.exports=app;
