5e28b04eed8b131e5dc098cc60cad42d09818f7f
BERQOUCH <5107270-NawelB@users.noreply.gitlab.com>
2020-12-14 10:45:36 +0100

    login and signUp

--- backend/controllers/User.js
+++ backend/controllers/User.js
@@ -27,9 +27,11 @@ exports.login= (req,res,next) => {
                     if (!valid){
                         return res.status(401).json({message: 'Mot de passe incorrect!'});
                     }
-                    
+                    res.status(200).json({
+                        userId : user._id,
+                        token: 'TOKEN',
+                    });
                 })
-                    
                 .catch(error => res.status(500).json({error}));
         })            
         .catch(error => res.status(500).json({error}))
