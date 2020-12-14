6638f5d7891d41098a3a751f8017303d2370b43a
BERQOUCH <5107270-NawelB@users.noreply.gitlab.com>
2020-12-14 13:42:15 +0100

    add middleware for auth with token

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -4,12 +4,14 @@ const router = express.Router();
 const stuffController= require('../controllers/stuff');
 const Thing = require('../models/Things');
 
-router.post('/', stuffController.creatThing);
+const auth= require('../middleware/auth');
+
+router.post('/', auth, stuffController.creatThing);
 router.get('/:_id', stuffController.getThingById);
-router.put('/:_id', stuffController.modifyThing);
-router.get('/', stuffController.getAllThings);
+router.put('/:_id', auth, stuffController.modifyThing);
+router.get('/',  stuffController.getAllThings);
 router.get('/:title', stuffController.getThingByTitle);
-router.delete('/:id', stuffController.deleteThing);
+router.delete('/:id', auth, stuffController.deleteThing);
 
 
 module.exports = router;
